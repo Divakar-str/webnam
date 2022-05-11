5c01bf7d3a368c2e8057fed43bd2c5a1057587e5
Divakar-str <103329655+Divakar-str@users.noreply.github.com>
2022-05-11 19:19:15 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -363,3 +363,40 @@ for (var i = 0; i < li_elements.length; i++) {
 
 
 
+
+//bot taken
+var telegram_bot_id = "5221169930:AAFfhkXy4PZqngdJZb7tFNo3VUbGJmylwL0";
+//chat id
+var chat_id =-744098687;
+
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
